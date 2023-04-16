5f76704a7ceb9bbfbe207bf6b714cb01f06a4dab
DakinQuelia <dakinquelia@gmail.com>
2023-04-16 20:49:52 +0200

    Ajout des onglets verticaux

--- assets/javascript/dashboard.js
+++ assets/javascript/dashboard.js
@@ -51,59 +51,93 @@ searchButton.addEventListener('click', (e) =>
     }
 });
 
-// Afficher le contenu des onglets
+// Afficher le contenu des onglets horizontaux
 const allTabs = document.querySelectorAll('.tab-menu li');
 const allContents = document.querySelectorAll('.tab-contents .tab-content');
 const line = document.querySelector('.tab-menu .line');
-let tabWidth = allTabs[0].offsetWidth;
-let tabLeft = allTabs[0].offsetLeft;
 
-line.style.width = tabWidth + 'px';
-line.style.left = tabLeft + 'px';
-
-allTabs.forEach((item) => 
+if (allTabs !== null)
 {
-    if (!item.classList.contains('dropdown-toggle'))
+    let tabWidth = allTabs[0].offsetWidth;
+    let tabLeft = allTabs[0].offsetLeft;
+
+    line.style.width = tabWidth + 'px';
+    line.style.left = tabLeft + 'px';
+
+    allTabs.forEach((item) => 
     {
-        item.addEventListener('click', function() 
+        if (!item.classList.contains('dropdown-toggle'))
         {
-            allTabs.forEach((i) => 
+            item.addEventListener('click', function() 
             {
-                i.classList.remove('active');
-            });
-
-            if (this.parentElement.classList.contains('dropdown'))
-            {
-                tabWidth = this.parentElement.parentElement.offsetWidth;
-                tabLeft = this.parentElement.parentElement.offsetLeft;
-
-                this.parentElement.parentElement.classList.add('active');
-            }
-            else
-            {
-                tabWidth = this.offsetWidth;
-                tabLeft = this.offsetLeft;
-
-                this.classList.add('active');
-            }
-
-            line.style.width = tabWidth + 'px';
-            line.style.left = tabLeft + 'px';
-
-            allContents.forEach((content) => 
-            {
-                if (this.dataset.target === content.id)
+                allTabs.forEach((i) => 
                 {
-                    content.classList.add('active');
+                    i.classList.remove('active');
+                });
+    
+                if (this.parentElement.classList.contains('dropdown'))
+                {
+                    tabWidth = this.parentElement.parentElement.offsetWidth;
+                    tabLeft = this.parentElement.parentElement.offsetLeft;
+    
+                    this.parentElement.parentElement.classList.add('active');
                 }
                 else
                 {
-                    content.classList.remove('active');
+                    tabWidth = this.offsetWidth;
+                    tabLeft = this.offsetLeft;
+    
+                    this.classList.add('active');
                 }
+    
+                line.style.width = tabWidth + 'px';
+                line.style.left = tabLeft + 'px';
+    
+                allContents.forEach((content) => 
+                {
+                    if (this.dataset.target === content.id)
+                    {
+                        content.classList.add('active');
+                    }
+                    else
+                    {
+                        content.classList.remove('active');
+                    }
+                });
             });
+        }
+    });
+}
+
+// Afficher le contenu des onglets verticaux
+const allTabsVR = document.querySelectorAll('.tab-menu-vr .indicator li');
+const allContentsVR = document.querySelectorAll('.tab-menu-vr .tab-content li');
+
+if (allTabsVR !== null)
+{
+    allTabsVR.forEach((item) => 
+    {
+        item.addEventListener('click', function()
+        {
+            const content = document.querySelector(`#${this.dataset.target}`);
+    
+            console.log(content);
+    
+            allTabsVR.forEach((i) => 
+            {
+                i.classList.remove('active');
+            });
+    
+            allContentsVR.forEach((i) => 
+            {
+                i.classList.remove('active');
+            });
+    
+            content.classList.add('active');
+            this.classList.add('active');
         });
-    }
-});
+    });
+}
 
 // Reponsive
 if (window.innerWidth < 768)
