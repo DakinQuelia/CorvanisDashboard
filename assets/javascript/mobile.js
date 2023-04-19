148c5417454a880ea4ddf67e057f61d9ea0da809
DakinQuelia <dakinquelia@gmail.com>
2023-04-19 04:46:59 +0200

    Ajout du javascript

--- assets/javascript/mobile.js
+++ assets/javascript/mobile.js
@@ -4,3 +4,17 @@
 *   Version     : 1.0.0.
 *   Auteur(s)   : Dakin Quelia <dakinquelia@gmail.com>
 **/
+/**
+*   Gestion des sous-menus
+**/
+const dropdownToggle = document.querySelector('.sidebar .nav-menu li a.dropdown-toggle');
+const dropdown = document.querySelector('.sidebar .nav-menu li .dropdown');
+
+dropdownToggle.addEventListener('click', function() 
+{
+    dropdown.classList.toggle('active');
+});
+
+/**
+*   Gestion de la barre de navigation
+**/
