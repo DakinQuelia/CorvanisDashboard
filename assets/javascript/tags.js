de554c2cc46bf30e8e24d8abf07acc81f36be11c
DakinQuelia <dakinquelia@gmail.com>
2023-04-23 02:32:25 +0200

    Mise à jour du code

--- assets/javascript/tags.js
+++ assets/javascript/tags.js
@@ -6,13 +6,18 @@
 **/
 const input = document.querySelector('.wrapper .tags .input-tag');
 const tagWrapper = document.querySelector('.wrapper .tags');
+const removeAll = document.querySelector('.bottom .remove-all');
+const totalTags = document.querySelector('.tags-wrap .bottom .total-tags');
 let listTag = [];
 
+/**
+*   On ajoute le tag à la liste 
+**/
 input.addEventListener('input', function()
 {
     if (this.value.includes(','))
     {
-        const str = this.value.toLowerCase().replace(/ +/g, ' ');
+        const str = this.value.toLowerCase().replace(/ +/g, ' ').split(',');
 
         str.forEach((item) => 
         { 
@@ -25,18 +30,44 @@ input.addEventListener('input', function()
             }
         });
 
+        getTagsCount();
+
         this.value = "";
     }
 });
 
 /**
+*   
+**/
+input.addEventListener('keydown', function(e)
+{
+    if (e.key === 'Backspace')
+    {
+        if (this.value === "")
+        {
+            const li = tagWrapper.querySelectorAll('li');
+
+            li[li.length - 1].remove();
+            this.value = listTag[listTag.length - 1] + " ";
+            listTag.pop();
+            
+            getTagsCount();
+        }
+    }
+});
+
+/**
 *   Cette fonction permet d'ajouter un tag à la liste.
+*
+*   @return {void}
 **/
 function addTag()
 {
+    tagWrapper.querySelectorAll('li').forEach(li => li.remove());
+
     listTag.forEach((item) => 
     { 
-        const li = document.createElemen('li');
+        const li = document.createElement('li');
 
         li.innerHTML = `
             <span class="text">${item}</span>
@@ -46,3 +77,42 @@ function addTag()
         tagWrapper.insertBefore(li, input);
     });
 }
+
+/**
+*   Cette fonction retourne la longueur du tableau des tags.
+*
+*   @return {void}
+**/
+function getTagsCount()
+{
+    totalTags.textContent = listTag.length;
+}
+
+/**
+*   On supprime le tag
+**/
+window.addEventListener('click', function(e) 
+{
+    if (e.target.matches('.bx.bx-x.close-button'))
+    {
+        const li = e.target.parentElement;
+        const text = li.querySelector('.text').textContent;
+
+        tagWrapper.removeChild(li);
+        listTag.splice(listTag.indexOf(text), 1);
+        
+        getTagsCount();
+    }
+});
+
+/**
+*   On supprime tous les tags
+**/
+removeAll.addEventListener('click', function(e) 
+{
+    tagWrapper.querySelectorAll('li').forEach(li => li.remove());
+
+    listTag = [];
+
+    getTagsCount();
+});
